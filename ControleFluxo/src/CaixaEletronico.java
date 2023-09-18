f48380895bac021bfb9cb246fba334d99d8e5fec
renanzip <renanzip@gmail.com>
2023-09-17 23:59:46 -0300

    laço if

---
+++ ControleFluxo/src/CaixaEletronico.java
@@ -0,0 +1,13 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        
+        double saldo = 25.00;
+        double valorSolicitado = 17.00;
+
+        if(valorSolicitado < saldo)
+            saldo = saldo - valorSolicitado;
+
+            System.out.println(saldo);
+
+    }
+}
