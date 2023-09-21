e8d89a50407b2612af29465d549c8c32abfc8ac8
renanzip <renanzip@gmail.com>
2023-09-21 00:15:01 -0300

    Atualização das atividades

--- ControleFluxo/src/CaixaEletronico.java
+++ ControleFluxo/src/CaixaEletronico.java
@@ -2,12 +2,17 @@ public class CaixaEletronico {
     public static void main(String[] args) {
         
         double saldo = 25.00;
-        double valorSolicitado = 17.00;
+        double valorSolicitado = 117.00;
 
-        if(valorSolicitado < saldo)
+        if(valorSolicitado < saldo){
             saldo = saldo - valorSolicitado;
+            System.out.println("Novo saldo: " + saldo);
+        }else{
+            System.out.println("Saldo insuficiente");
 
-            System.out.println(saldo);
+        System.out.println(saldo);
+        }
+       
 
     }
 }
